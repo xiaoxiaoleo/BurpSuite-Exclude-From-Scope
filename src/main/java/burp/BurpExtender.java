9369bcc57327a730aa4d1595ac3a840568d673fb
Coray Li <coray.li@ingka.com>
2020-11-05 11:12:36 +0800

    first commit

---
+++ src/main/java/burp/BurpExtender.java
@@ -0,0 +1,116 @@
+package burp;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JMenuItem;
+
+
+public class BurpExtender implements IBurpExtender, IContextMenuFactory {
+
+    private IBurpExtenderCallbacks callbacks;
+    private static PrintWriter stdout;
+    private static boolean isEnable = true;
+    IExtensionHelpers helpers = null;
+
+    protected static String E_TOP_DOMIAIN = "Exclude Top Domain From Scope";
+    protected static String E_CUR_DOMIAIN = "Exclude Current Domain From Scope";
+    protected static String E_CUR_URL = "Exclude Current URL From Scope";
+
+    @Override
+    public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks)
+    {
+        this.callbacks = callbacks;
+        this.helpers = callbacks.getHelpers();
+
+        callbacks.setExtensionName("Exclude from Scope");
+
+
+        stdout = new PrintWriter(callbacks.getStdout(), true);
+        stdout.println("Author: xiaoxiaoleo");
+        stdout.println("Repo: https://github.com/xiaoxiaoleo/BurpSuite-Exclude-From-Scope");
+
+        callbacks.registerContextMenuFactory(BurpExtender.this);
+    }
+
+    @Override
+    public List<JMenuItem> createMenuItems(IContextMenuInvocation invocation) {
+        if(invocation.getInvocationContext() == invocation.CONTEXT_PROXY_HISTORY){
+            if(true) {
+
+                //String curURL = iURL.getProtocol() + "://"+  iURL.getHost()  + "/" +  iURL.getPath();
+                //String topDomain = iURL.getHost().split(".")[-2].concat(".").concat(iURL.getHost().split(".")[-1]);
+
+                List<JMenuItem> menu = new ArrayList<JMenuItem>();
+                //JMenuItem jItem1 = new JMenuItem(E_TOP_DOMIAIN + ": " + iURL.toString());
+                JMenuItem jItem2 = new JMenuItem(E_CUR_DOMIAIN);
+                //JMenuItem jItem3 = new JMenuItem(E_CUR_URL + ": " + iURL.toString());
+                //menu.add(jItem1);
+                menu.add(jItem2);
+                //menu.add(jItem3);
+
+                jItem2.addActionListener(new ActionListener(){
+
+                    public void actionPerformed(ActionEvent arg0) {
+                        IHttpRequestResponse req = invocation.getSelectedMessages()[0];
+
+                        IRequestInfo request = helpers.analyzeRequest(req.getRequest());
+                        String host = "fail.get.host";
+                        for(String headLine : request.getHeaders()){
+                            if (headLine.toLowerCase().startsWith("host")){
+                                host = headLine.split(": ")[1];
+                            }
+
+                        }
+
+                        String curDomain = "https" + "://"+ host;
+                        String curDomainHTTP = "http" + "://"+ host;
+                        URL url, urlHTTP;
+                        try {
+                            url = new URL(curDomain);
+                            callbacks.excludeFromScope(url);
+                            urlHTTP = new URL(curDomainHTTP);
+                            callbacks.excludeFromScope(urlHTTP);
+                        } catch (MalformedURLException ex) {
+
+                        }
+                    }
+                });
+
+
+                return menu;
+            }
+        }
+        return null;
+    }
+
+/*
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            if (e.getActionCommand().startsWith("Exclude ")) {
+                URL url;
+                try {
+                    url = new URL(e.getActionCommand().split(": ")[1]);
+                    this.callbacks.excludeFromScope(url);
+                } catch (MalformedURLException ex) {
+
+                }
+
+            }
+        }catch(Exception exec){
+
+        }
+
+    }
+*/
+
+
+
+
+}
